ed9e81d8b99b148ed42e06e1c84e1f3a0cf1c7f0
Selwes13 <samelwes@btinternet.com>
2017-02-12 23:44:54 +0000

    Create sort.h

---
+++ sort.h
@@ -0,0 +1,21 @@
+class sort {
+public:
+	static void print(int* arr, int start, int end);
+	static void print(int* arr, int start, int end, int piv);
+
+	static int* bubbleSort(int* arr, int len);
+	static int* insersionSort(int* arr, int len);
+	static int* selectionSort(int* arr, int len);
+	static int* quickSort(int* arr, int len);
+	static int* heapSort(int* arr, int len);
+	static int* mergeSort(int* arr, int len);
+
+	static int* invert(int* arr, int len);
+private:
+	static int* QSPartition(int* arr, int start, int end);
+
+	static int* maxHeapify(int* arr, int len, int end);
+public:
+	static int* merge(int* arr1, int len1, int* arr2, int len2);
+	static int* partArray(int* fullArray, int from, int to);
+};
